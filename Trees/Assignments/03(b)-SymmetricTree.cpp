0de918d3945591cc41915a50f8a4ab2838641b91
Devesh Tuli <deveshtuli02@gmail.com>
2024-11-07 14:34:24 +0530

    Create 03(b)-SymmetricTree.cpp

---
+++ Trees/Assignments/03(b)-SymmetricTree.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+bool isMirror(TreeNode* p, TreeNode* q) {
+        if(!p&&!q){
+            return true;
+        }
+        else if(p&&q){
+            return (p->val==q->val)
+            && isMirror(p->left,q->right)
+            &&isMirror(p->right,q->left);
+            
+             
+        }
+        else{
+            return false;
+        }
+    }
+    bool isSymmetric(TreeNode* root) {
+        return isMirror(root->left,root->right);
+        
+    }
+};
