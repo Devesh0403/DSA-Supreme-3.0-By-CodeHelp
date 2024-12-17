83a43626c2e083294c714874c450f727e315cc3c
Devesh Tuli <deveshtuli02@gmail.com>
2024-12-17 17:58:37 +0530

    Update 07(b)UniqueBST2.cpp

--- DP /Assignments/07(b)UniqueBST2.cpp
+++ DP /Assignments/07(b)UniqueBST2.cpp
@@ -37,6 +37,34 @@ public:
         }
         return ans;
     }
+    map<pair<int,int>,vector<TreeNode*>>mp;
+    vector<TreeNode*> memoization(int start, int end) {
+        if (start > end)
+            return  {NULL};
+        if (start == end) {
+            TreeNode* root = new TreeNode(start);
+            return mp[{start,end}]={root};
+        }
+
+        if(mp.find({start,end})!=mp.end())return mp[{start,end}];
+
+        vector<TreeNode*> ans;
+        for (int i = start; i <= end; i++) {
+            vector < TreeNode*> leftBST = memoization(start, i - 1);
+            vector < TreeNode*> rightBST = memoization(i + 1, end);
+
+            for (TreeNode* leftRoot : leftBST) {
+                for (TreeNode* rightRoot : rightBST) {
+                    TreeNode* root = new TreeNode(i);
+                    root->left = leftRoot;
+                    root->right = rightRoot;
+
+                    ans.push_back(root);
+                }
+            }
+        }
+        return mp[{start,end}]= ans;
+    }
     vector<TreeNode*> generateTrees(int n) {
         return solve(1,n);
     }
