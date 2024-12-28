f1dfa3c5b397b67b19ea123a3710696a9778a884
Devesh Tuli <deveshtuli02@gmail.com>
2024-12-28 14:17:37 +0530

    Update 01-CycleDetection.cpp

--- Graph/Class 2/01-CycleDetection.cpp
+++ Graph/Class 2/01-CycleDetection.cpp
@@ -1,93 +1,15 @@
-// Online C++ compiler to run C++ program online
-#include <iostream>
+//{ Driver Code Starts
+#include <bits/stdc++.h>
 using namespace std;
-#include <list>
-#include <unordered_map>
-#include <queue>
 
-class Graph{
-    public:
-    unordered_map<int,list<int>>adjList;
-    
-    void addEdge(int u,int v, bool direction){
-        //direction ==0 =>undirected
-        //direction ==1 =>directed
-        if(direction==0){
-            adjList[u].push_back(v);
-            adjList[v].push_back(u);
-        }
-        else{
-            adjList[u].push_back(v);
-        }
-    }
-    
-    // void printList(int n){
-    //     for(int i=0;i<n;i++){
-    //         cout<<i<<": {";
-    //         list<int>temp=adjList[i];
-    //         for(auto j:temp){
-    //             cout<<"("<<j.first<<","<<j.second<<")";
-    //         }
-    //         cout<<"}"<<endl;
-    //     }
-    // }
-    void BFSTraversal(int src){
-        queue<int>q;
-        unordered_map<int,bool>visited;
-        
-        q.push(src);
-        visited[src]=true;
-        
-        while(!q.empty()){
-            int frontNode=q.front();
-            cout<<frontNode<<" ";
-            q.pop();
-            
-            for(auto neighbour:adjList[frontNode]){
-                
-                int node=neighbour;
-                
-                
-                if(!visited[node]){
-                    q.push(node);
-                    visited[node]=true;
-                }
-            }
-            
-        }
-    }
-    void DFSHelper(int src,unordered_map<int,bool>&visited){
-        visited[src]=true;
-        cout<<src<<",";
-        
-        for(auto nbr:adjList[src]){
-            //nbr-pair
-            int node=nbr;
-            if(!visited[node]){
-                DFSHelper(node,visited);
-                
-                
-            }
-            
-        }
-    }
-    
-    void DFSTraversal(int n){
-        
-       unordered_map<int,bool>visited;
-       for(int src=0;src<n;src++){
-           if(!visited[src]){
-               DFSHelper(src,visited);
-           }
-       }
-        
-        // DFSHelper(src,visited);
-        
-    }
-    
-    bool cycleDetectionUndirectedBFS(int src){
+
+// } Driver Code Ends
+class Solution {
+  public:
+    bool cycleDetectionUndirectedBFS(int src, unordered_map<int,bool>&visited,
+    vector<vector<int>>& adjList){
         queue<pair<int,int>>q;
-        unordered_map<int,bool>visited;
+       
         
         q.push({src,-1});
         visited[src]=true;
@@ -112,43 +34,50 @@ class Graph{
         }
         return false;
     }
-    
+    bool isCycle(vector<vector<int>>& adj) {
+        
+         unordered_map<int,bool>visited;
+         bool ans=false;
+         
+         int v=adj.size();
+         for(int i=0;i<v;i++){
+             if(!visited[i]){
+                 ans=cycleDetectionUndirectedBFS(i,visited,adj);
+                 if(ans==true)break;
+                 
+             }
+         }
+         return ans;
+        
+        
+        // Code here
+    }
 };
 
+//{ Driver Code Starts.
 int main() {
-    
-    Graph g;
-    g.addEdge(0,2,0);
-    g.addEdge(0,3,0);
-    g.addEdge(0,5,0);
-    g.addEdge(3,5,0);
-    g.addEdge(2,5,0);
-    g.addEdge(5,6,0);
-    g.addEdge(5,4,0);
-    g.addEdge(4,1,0);
-    g.addEdge(6,1,0);
-    
-    
-    // g.printList(7);
-    
-    // cout<<"BFS Traversal"<<endl;
-    // g.BFSTraversal(0);
-    
-    cout<<"DFS Traversal"<<endl;
-    
-    g.DFSTraversal(7);
-    
-    bool hasCycle = g.cycleDetectionUndirectedBFS(1);
-if (hasCycle) {
-    cout << "Cycle detected." << endl;
-} else {
-    cout << "No cycle detected." << endl;
-}
-    
-    
-    
-    
-    
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int V, E;
+        cin >> V >> E;
+        vector<vector<int>> adj(V);
+        for (int i = 0; i < E; i++) {
+            int u, v;
+            cin >> u >> v;
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        Solution obj;
+        bool ans = obj.isCycle(adj);
+        if (ans)
+            cout << "1\n";
+        else
+            cout << "0\n";
 
+        cout << "~"
+             << "\n";
+    }
     return 0;
 }
+// } Driver Code Ends
