ccf477263b426d1e4b0df616801822fa02e2d8ba
Devesh Tuli <deveshtuli02@gmail.com>
2024-12-29 13:35:20 +0530

    Create 02-TopologicalSortBFS.cpp

---
+++ Graph/Class 3/02-TopologicalSortBFS.cpp
@@ -0,0 +1,45 @@
+class Solution {
+  public:
+
+    void topoBFS(int n,vector<vector<int>>& adj,vector<int>&ans){
+        unordered_map<int,int>indegree;
+        queue<int>q;
+        
+        for(int src=0;src<n;src++){
+            for(auto nbr:adj[src]){
+                indegree[nbr]++;
+            }
+        }
+        
+        for(int i=0;i<n;i++){
+            if(indegree[i]==0){
+                q.push(i);
+            }
+        }
+        
+        while(!q.empty()){
+            int front=q.front();
+            ans.push_back(front);
+            
+            for(auto nbr:adj[front]){
+                indegree[nbr]--; 
+                if(indegree[nbr]==0){
+                    q.push(nbr);
+                }
+            }
+            q.pop();
+            
+        }
+    }
+    vector<int> topologicalSort(vector<vector<int>>& adj) {
+        
+        vector<int>ans;
+        topoBFS(adj.size(),adj,ans);
+        return ans;
+        
+        // Your code here
+    }
+};
+
+
+
