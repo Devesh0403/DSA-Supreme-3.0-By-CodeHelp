f28b90a59e1d144182bdbd989fdde1ffad3b53ac
Devesh Tuli <deveshtuli02@gmail.com>
2025-01-15 22:42:40 +0530

    Create 02-PrintLinkedList.cpp

---
+++ Linked Lists/Class 1/02-PrintLinkedList.cpp
@@ -0,0 +1,34 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node* next;
+    
+    Node(int data1,Node* next1){
+        data=data1;
+        next=next1;
+    }
+    Node (int data1){
+        data=data1;  
+        next=nullptr; 
+
+    }
+    
+};
+void printLL(Node* head) {
+    while (head != NULL) {
+        cout << head->data << " ";
+        head = head->next;
+    }
+}
+
+int main() {
+    Node* head= new Node(0);
+    head->next=new Node(1);
+    head->next->next=new Node(2);
+    head->next->next->next=new Node(3);
+    
+    printLL(head);
+}
