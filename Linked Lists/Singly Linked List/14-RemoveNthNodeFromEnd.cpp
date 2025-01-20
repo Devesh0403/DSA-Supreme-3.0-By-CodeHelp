bf806196499ff1cc3617c1654f5c5684da7b7f9c
Devesh Tuli <deveshtuli02@gmail.com>
2025-01-20 13:11:59 +0530

    update

--- Linked Lists/Singly Linked List/14-RemoveNthNodeFromEnd.cpp
+++ Linked Lists/Singly Linked List/14-RemoveNthNodeFromEnd.cpp
@@ -1,13 +1,4 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+// METHOD -1 Find Length of Linked List
 class Solution {
 public:
 int getLength(ListNode* head){
@@ -39,6 +30,31 @@ int getLength(ListNode* head){
         
         return head;
 
+        
+    }
+};
+
+// METHOD -2 Using Slow & Fast pointers
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode* dummy=new ListNode(0);
+        dummy->next=head;
+
+        ListNode* fast=dummy;
+        ListNode* slow=dummy;
+
+        while(n--){
+            fast=fast->next;
+
+        }
+        while(fast->next!=NULL){
+            fast=fast->next;
+            slow=slow->next;
+
+        }
+        slow->next=slow->next->next;
+        return dummy->next;
         
     }
 };
