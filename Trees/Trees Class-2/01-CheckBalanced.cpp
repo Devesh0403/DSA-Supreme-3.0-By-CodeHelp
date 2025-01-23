b2c1dac4676c925769690fb1e96ba26ceb856c95
Devesh Tuli <deveshtuli02@gmail.com>
2025-01-23 20:42:10 +0530

    Update 01-CheckBalanced.cpp

--- Trees/Trees Class-2/01-CheckBalanced.cpp
+++ Trees/Trees Class-2/01-CheckBalanced.cpp
@@ -1,51 +1,31 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+
 class Solution {
 public:
     int getHeight(TreeNode* root){
-        if(root==NULL){
-            return 0;
-        }
-
-        int leftHeight=getHeight(root->left);
-        int rightHeight=getHeight(root->right);
-
-        return max(leftHeight,rightHeight)+1;
-
-    }
-    
-    bool isBalanced(TreeNode* root) {
-        if(root==NULL){
-            return true;
-        }
+        if(!root)return 0;
 
         int lh=getHeight(root->left);
         int rh=getHeight(root->right);
 
-        int ansDiff=abs(lh-rh);
-        bool status=ansDiff<=1;
+        return max(lh,rh)+1;
 
-        bool leftAns=isBalanced(root->left);
-        bool rightAns=isBalanced(root->right);
+    }
+    bool isBalanced(TreeNode* root) {
 
-        if(status&&leftAns&&rightAns){
+        if(!root)return true;
+
+        int leftHeight=getHeight(root->left);
+        int rightHeight=getHeight(root->right);
+
+        if (abs(leftHeight - rightHeight) <= 1 &&
+            isBalanced(root->left) &&
+            isBalanced(root->right)) {
             return true;
         }
-        else{
-            return false;
-        }
-        
 
-        
+        return false;
+
+
         
     }
 };
