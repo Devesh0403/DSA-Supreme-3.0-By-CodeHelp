b597e9a6ddb2f0f0e04495169e8c57fe11041c96
Devesh Tuli <deveshtuli02@gmail.com>
2025-01-23 19:39:18 +0530

    Update 03-LevelOrderTraversal.cpp

--- Trees/Trees Class-1/03-LevelOrderTraversal.cpp
+++ Trees/Trees Class-1/03-LevelOrderTraversal.cpp
@@ -101,6 +101,62 @@ void levelOrderTraversalWithEOL(Node* root){
         
     }
 }
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>>ans;
+        if (root == nullptr) {
+            // If the tree is empty,
+            // return an empty vector
+            return ans; 
+        }
+
+        
+
+        queue<TreeNode*>q;
+        q.push(root);
+
+        while(!q.empty()){
+            int size=q.size();
+            vector<int> level; 
+
+            for(int i=0;i<size;i++){
+                TreeNode* node = q.front(); 
+
+                // Remove the front node from the queue
+                q.pop(); 
+
+                // Store the node value in the current level vector
+                level.push_back(node->val); 
+
+                if (node->left != nullptr) {
+                    q.push(node->left);
+                }
+                if (node->right != nullptr) {
+                    q.push(node->right);
+                }
+
+            }
+            ans.push_back(level);
+
+
+            
+        }
+        return ans;
+        
+    }
+};
 
 int main() {
     Node * root;
