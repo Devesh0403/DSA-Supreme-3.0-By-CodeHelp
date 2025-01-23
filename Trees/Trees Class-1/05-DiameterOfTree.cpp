61cf40afbe95320d838e9ac39d99bdbf446843ee
Devesh Tuli <deveshtuli02@gmail.com>
2025-01-23 20:49:33 +0530

    Update 05-DiameterOfTree.cpp

--- Trees/Trees Class-1/05-DiameterOfTree.cpp
+++ Trees/Trees Class-1/05-DiameterOfTree.cpp
@@ -1,37 +1,23 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    int getDiameter(TreeNode* root,int &diameter){
-        if(root==NULL){
-            return 0;
-        }
-        int leftHeight=getDiameter(root->left,diameter);
-        int rightHeight=getDiameter(root->right,diameter);
+    void diameter(TreeNode* root,int& diameter){
+        if(root==NULL)return 0;
 
-        diameter=max(diameter,leftHeight+rightHeight);
+        int lh=diameter(root,diameter);
+        int rh=diameter(root,diameter);
+
+        diameter=max(diameter,rh+lh);
         
-        int maxHeight=max(leftHeight,rightHeight);
-        return maxHeight+1;
+        return max(lh,rh)+1;
     }
     int diameterOfBinaryTree(TreeNode* root) {
-
         if(root==NULL){
             return 0;
         }
+
         int diameter=0;
-        getDiameter(root,diameter);
+        diameter(root,diameter);
         return diameter;
-        
         
     }
 };
