463da4a8a9baf99504f0e5fb803d6f9cdcfeb745
Devesh Tuli <deveshtuli02@gmail.com>
2025-02-02 20:43:25 +0530

    Update 04-BinarySubarraysWithSum.cpp

--- Sliding Window/Medium/04-BinarySubarraysWithSum.cpp
+++ Sliding Window/Medium/04-BinarySubarraysWithSum.cpp
@@ -20,3 +20,37 @@ public:
 };
 // TC- O(N)
 // SC- O(N) [MAP]
+
+// 2 POINTER APPROACH
+class Solution {
+public:
+int check(vector<int>& nums,int goal){
+        if(goal<0)return 0;
+
+        int left=0;
+        int right=0;
+        int cnt=0;
+        int sum=0;
+
+        while(right<nums.size()){
+            sum+=nums[right];
+            while(sum>goal){
+                sum-=nums[left];
+                left++;
+            }
+            if(sum<=goal)cnt+=(right-left+1);
+            right++;
+
+
+        }
+        return cnt;
+    }
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        return check(nums,goal)-check(nums,goal-1);
+        
+        
+    }
+};
+
+// TC - O(2*N)
+// SC - O(1)
