e99ccce6114db04802eb7c4c4fd4bfb486e573b8
Devesh Tuli <deveshtuli02@gmail.com>
2025-02-02 20:47:46 +0530

    Update 05-NumberOfNiceSubarrays.cpp

--- Sliding Window/Medium/05-NumberOfNiceSubarrays.cpp
+++ Sliding Window/Medium/05-NumberOfNiceSubarrays.cpp
@@ -25,3 +25,38 @@ public:
 
 // TC- O(N^2)
 // SC- O(1)
+
+// METHOD 2- SAME AS NUMBER OF BINARY SUBARRAYS WITH SUM K
+
+class Solution {
+public:
+int check(vector<int>& nums,int goal){
+        if(goal<0)return 0;
+
+        int left=0;
+        int right=0;
+        int cnt=0;
+        int sum=0;
+
+        while(right<nums.size()){
+            // CONSIDER ODD AS 1 AND EVEN AS 0
+            sum+=nums[right]%2;
+            while(sum>goal){
+                sum-=nums[left]%2;
+                left++;
+            }
+            if(sum<=goal)cnt+=(right-left+1);
+            right++;
+
+
+        }
+        return cnt;
+    }
+    int numberOfSubarrays(vector<int>& nums, int goal) {
+        return check(nums,goal)-check(nums,goal-1);
+        
+        
+    }
+};
+// TC - O(2*N)
+// SC - O(1)
